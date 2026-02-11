f177b69d34bce07041fc2dbabd140412fd2d1bd7
Zahran Sammi <2020169@man9-jkt.sch.id>
2026-02-11 22:20:47 +0700

    Checkpoint from VS Code for cloud agent session

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,6 @@
 export function Card(namaBrand, jenis, transmisi, cc) {
     return `
-    <div class="flex flex-col bg-white mt-8 pb-4 border border-gray-300 shadow-2xl w-64 rounded-lg overflow-hidden">
+    <div class="flex flex-col bg-white mt-8 pb-4 border border-gray-300 shadow-2xl w-64 rounded-lg ">
     <div class="border-b-2 border-gray-300 px-4 py-2">
     <h1 class="font-semibold">${namaBrand}</h1>
     </div>
